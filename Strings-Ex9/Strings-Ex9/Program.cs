a018b028acce68d2aa6406ee309c6c9358fcd0db
MariusA <anca.ticau@gmail.com>
2019-06-06 23:14:47 +0300

    Exercise 9

---
+++ Strings-Ex9/Strings-Ex9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Strings_Ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string removeSpaces = RemoveSpaces("  This is a string with   spaces!");
+
+            Console.WriteLine(removeSpaces);
+        }
+
+        //Write a method to remove spaces from a given string.
+
+        public static string RemoveSpaces(string input)
+        {
+            string result = input.Replace(" ", String.Empty);
+
+            return result;
+        }
+    }
+}
